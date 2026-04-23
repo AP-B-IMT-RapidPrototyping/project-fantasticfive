924488a16672dc6d418304e10e5590036c592963
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-04-23 22:56:25 +0200

    Added visual radiation

--- ironCovenant/scripts/npc_logic/TrainSpotlight.cs
+++ ironCovenant/scripts/npc_logic/TrainSpotlight.cs
@@ -8,7 +8,17 @@ public partial class TrainSpotlight : Node3D
 {
 	[Export] private Player player;
 	[Export] private Timer visionTimer;
+	[Export] private Timer bufferTimer;
+	[Export] private Timer radiationTick;
 	[Export] private ShapeCast3D shapeCast;
+
+	[Export] private ShaderMaterial _shader;
+	[Export] private ColorRect _shaderRect;
+
+	private double intensity = 0;
+	private double baseIntensity = 1;
+	private bool beingRadiated = false;
+	public bool canRadiate = false;
 
 	public override void _Ready()
 	{
@@ -17,6 +27,9 @@ public partial class TrainSpotlight : Node3D
 		{
 			GD.Print("No player found");
 		}
+		_shaderRect.Visible = false;
+		bufferTimer.Timeout += StartRadiation;
+		radiationTick.Timeout += AddRadiation;
 	}
 
 	
@@ -37,34 +50,71 @@ public partial class TrainSpotlight : Node3D
 
 	private void CheckVision()
 	{
-		shapeCast.ForceShapecastUpdate();
-		if (shapeCast.IsColliding())
+		if (canRadiate)
 		{
-			var results = new List<(GodotObject collider, float distance)>();
-			Vector3 origin = shapeCast.GlobalTransform.Origin;
-
-			for (int i = 0; i < shapeCast.GetCollisionCount(); i++)
+			shapeCast.ForceShapecastUpdate();
+			if (shapeCast.IsColliding())
 			{
-				var collider = shapeCast.GetCollider(i);
-				Vector3 point = shapeCast.GetCollisionPoint(i);
+				var results = new List<(GodotObject collider, float distance)>();
+				Vector3 origin = shapeCast.GlobalTransform.Origin;
 
-				float dist = origin.DistanceTo(point);
-
-				results.Add((collider, dist));
-			}
-
-			results.Sort((a, b) => a.distance.CompareTo(b.distance));
-
-			if (results[0].collider is Node3D node)
-			{
-				if (node.Name == "Player" || node.Name == "TrainCollision" || node.Name == "Floor")
+				for (int i = 0; i < shapeCast.GetCollisionCount(); i++)
 				{
-					GD.Print("Player is infront");
-				} else
+					var collider = shapeCast.GetCollider(i);
+					Vector3 point = shapeCast.GetCollisionPoint(i);
+
+					float dist = origin.DistanceTo(point);
+
+					results.Add((collider, dist));
+				}
+
+				results.Sort((a, b) => a.distance.CompareTo(b.distance));
+
+				if (results[0].collider is Node3D node)
 				{
-					GD.Print($"Player is not infront, blocked by {node.Name}");
+					if (node.Name == "Player" || node.Name == "TrainCollision" || node.Name == "Floor")
+					{
+						//player is infront
+						if (beingRadiated)
+						{
+							StopRadiation();
+							beingRadiated = false;
+						}
+					}
+					else
+					{
+						//player is behind
+						if (!beingRadiated)
+						{
+							beingRadiated = true;
+							bufferTimer.Start();
+						}
+					}
 				}
 			}
 		}
+	}
+
+	private void StartRadiation()
+	{
+		radiationTick.Start();
+		intensity = baseIntensity;
+		_shader.SetShaderParameter("intensity", intensity);
+		_shaderRect.Visible = true;
+		GD.Print("Start radiation");
+	}
+
+	private void StopRadiation()
+	{
+		radiationTick.Stop();
+		bufferTimer.Stop();
+		_shaderRect.Visible = false;
+		GD.Print("Stop radiation");
+	}
+
+	private void AddRadiation()
+	{
+		intensity += 0.01;
+		_shader.SetShaderParameter("intensity", intensity);
 	}
 }

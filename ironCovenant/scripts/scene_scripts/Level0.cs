3716517a22871759a4f576c5ba2a5b4a1cfec669
CasuallySleepy <145465388+CasuallySleepy@users.noreply.github.com>
2026-04-26 00:50:54 +0200

    Level0 created, Cutscene trigger works (kinda), yeah, cutscene stuff :)

---
+++ ironCovenant/scripts/scene_scripts/Level0.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+public partial class Level0 : Node3D
+{
+    [Export] private AnimationPlayer _cutsceneIntroAnim;
+    [Export] private AnimationPlayer _cutsceneAnim;
+
+    [Export] private Camera3D _cutsceneCamera;
+
+
+
+
+    private void OnCutsceneTrigger(Node3D body)
+    {
+        if (body.IsInGroup("player"))
+        {
+            GD.Print("yes " + body.Name);
+            _cutsceneCamera.Current = true;
+            _cutsceneIntroAnim.Play("play");
+        }
+    }
+
+    private void OnIntroFinished(StringName anim)
+    {
+        _cutsceneAnim.Play("play");
+    }
+
+    private void OnCutsceneFinished(StoreName anim)
+    {
+        _cutsceneCamera.Current = false;
+    }
+}

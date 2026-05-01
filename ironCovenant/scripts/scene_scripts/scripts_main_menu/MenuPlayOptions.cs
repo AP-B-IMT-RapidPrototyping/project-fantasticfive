7e1bb657f1d5ec8c7f3b4e772ae9fbe7d7f9a44a
CasuallySleepy <145465388+CasuallySleepy@users.noreply.github.com>
2026-05-01 20:28:27 +0200

    train moves and slows down when entering level 1, switching between main menu and level 1 now uses the SceneManager

--- ironCovenant/scripts/scene_scripts/scripts_main_menu/MenuPlayOptions.cs
+++ ironCovenant/scripts/scene_scripts/scripts_main_menu/MenuPlayOptions.cs
@@ -1,12 +1,22 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public partial class MenuPlayOptions : Control
 {
+    SceneManager sceneManager;
+
     [Export] private AnimationPlayer _animCamSwitch;
 
     [Export] private PackedScene _gameScene;
     [Export] private PackedScene _newGameScene;
+
+
+    public override void _Ready()
+    {
+        sceneManager = GetNode<SceneManager>("/root/SceneManager");
+    }
+
 
     private void OnContinuePressed()
     {
@@ -18,14 +28,19 @@ public partial class MenuPlayOptions : Control
         GetTree().ChangeSceneToPacked(_gameScene);
     }
 
-    private void OnNewGamePressed()
+    private async void OnNewGamePressed()
+    {
+        await NewGame();
+    }
+
+    private async Task NewGame()
     {
         if (_newGameScene == null)
         {
             GD.PrintErr($"Please assign scenes in the {Name} inspector.");
             return;
         }
-        GetTree().ChangeSceneToPacked(_newGameScene);
+        await sceneManager.SwitchScene(_newGameScene.ResourcePath);
     }
 
     private void OnBackPressed()
